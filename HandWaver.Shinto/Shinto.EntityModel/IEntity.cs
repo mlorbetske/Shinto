a282cee14250be6ad4aaaafc064816950b4a153f
Damon Payne <damon>
2012-12-07 20:58:27 -0600

    Testing new .gitignore

--- HandWaver.Shinto/Shinto.EntityModel/IEntity.cs
+++ HandWaver.Shinto/Shinto.EntityModel/IEntity.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// http://creativecommons.org/licenses/by/3.0/
+// Please consider giving me a link or shout out if you use this code 
+// http://www.damonpayne.com
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +9,22 @@ using System.Text;
 namespace Shinto.EntityModel
 {
     /// <summary>
-    /// Testing Github for Windows
+    /// An Entity has a key representing its identity. Implementors should consider overriding Equals and GetHashCode
+    /// in terms of this key
     /// </summary>
-    public class IEntity
+    public interface IEntity
     {
+        /// <summary>
+        /// Return the Id
+        /// </summary>
+        /// <returns></returns>
+        object GetId();
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">New Id value</param>
+        void SetId(object value);
     }
 }
---
+++ HandWaver.Shinto/Shinto.EntityModel/IEntityT.cs
@@ -0,0 +1,18 @@
+﻿// http://creativecommons.org/licenses/by/3.0/
+// Please consider giving me a link or shout out if you use this code 
+// http://www.damonpayne.com
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shinto.EntityModel
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TId">The id type, often a value type</typeparam>
+    public interface IEntity<TId> : IEntity
+    {
+    }
+}
