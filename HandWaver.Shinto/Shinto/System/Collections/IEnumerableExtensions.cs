83b326e98245ae98fd3ac90da89412b8383d2dae
Damon Payne <damon@myfirstandlastname.com>
2013-02-14 17:35:05 -0600

    Multi-computer same repo test

---
+++ HandWaver.Shinto/Shinto/System/Collections/IEnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shinto.System.Collections
+{
+    public static class IEnumerableExtensions
+    {
+    }
+}
